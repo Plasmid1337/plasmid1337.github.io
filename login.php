0fcc04ba876698b1efa8947414874a78937d9013
Plasmid1337 <Plasmid1337@gmail.com>
2016-09-30 00:35:13 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,14 @@
-# thing for the thing
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+    $usersName = $_POST["username"];
+    $password = $_POST["password"];
+    
+    if (username=="guest" && password == "guest"){
+    echo "success";
+    }else{echo "failure";}
+    ?>
+  </body>
+</html>
